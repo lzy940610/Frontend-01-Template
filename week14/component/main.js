0b137caa9876b65a4159eefbeb48abcd9f9f3111
lizhenyu <985818479@qq.com>
2020-07-15 21:53:03 +0800

    第十四周作业
    1、组件构建
    2、Carousel

--- week14/component/main.js
+++ week14/component/main.js
@@ -1,105 +1,75 @@
-function createElement(Cls, attributes, ...children) {
-  // console.log('arguments', arguments)
-  let o
+import { createElement, Text, Wrapper } from './createElement'
 
-  if (typeof Cls === "string") {
-    o = new Wrapper(Cls)
-  } else {
-    o = new Cls({
-      timer: {}
-    })
-  }
-
-  for (const name in attributes) {
-    // o[name] = attributes[name]
-    // o[name] = attributes[name]
-    o.setAttribute(name, attributes[name])
-  }
-
-  for (let child of children) {
-    if (typeof child === "string")
-      child = new Text(child)
-    o.appendChild(child)
-  }
-
-  // 在JSX中父子组件的构建顺序为 ： 先子后父
-
-  // console.log('children', children)
-  return o
-}
-
-class Text {
-  constructor(text) {
-    this.root = document.createTextNode(text)
-  }
-  mountTo(parent) {
-    parent.appendChild(this.root)
-  }
-}
-
-class Wrapper {
-  constructor(type) {
-    this.children = []
-    this.root = document.createElement(type)
-    // console.log('config', config)
-  }
-  setAttribute(name, value) { // attribute
-    // console.log('setAttribute :', name, value)
-    this.root.setAttribute(name, value)
-  }
-  appendChild(child) { // children
-    // console.log('appendChild :', child)
-    // child.mountTo(this.root)
-    this.children.push(child)
-  }
-  mountTo(parent) { // mount 是生命周期 一些DOM挂载最好也放入里面
-    parent.appendChild(this.root)
-    for (const child of this.children) {
-      child.mountTo(this.root)  
-    }
-  }
-}
-
-class MyComponent {
+class Carousel {
   constructor(config) {
     this.children = []
-    // this.slot = document.createElement("div")
-    // console.log('config', config)
+    this.attributes = new Map()
+    this.properties = new Map()
   }
   setAttribute(name, value) { // attribute
-    // console.log('setAttribute :', name, value)
-    this.root.setAttribute(name, value)
+    this[name] = value
   }
-  appendChild(child) { // children
-    // console.log('appendChild :', child)
-    // child.mountTo(this.root)
-    // this.children.push(child)
-    // this.slot.appendChild(child)
-    this.children.push(child)
+  addEventListener() {
+    this.root.addEventListener(...arguments)
   }
   render() {
-    return <article>
-      <header>I'am a header</header>
-      {this.slot}
-      <footer>I'm a footer</footer>
-    </article>
+    let position = 0
+
+    let nextPic = () => {
+      // loop 技巧 整数范围内的循环
+      let nextPosition = (position + 1) % this.data.length
+
+      let current = this.root.childNodes[position]
+      let next = this.root.childNodes[nextPosition]
+
+      current.style.transition = "ease 0s"
+      next.style.transition = "ease 0s"
+
+      current.style.transform = `translateX(${- 100 * position}%)`
+      next.style.transform = `translateX(${100 - 100 * nextPosition}%)`
+
+      // 使用Broswer的 setTimeout延时执行
+      setTimeout(() => {
+
+        current.style.transition = "" // means use css rule
+        next.style.transition = ""
+        current.style.transform = `translateX(${- 100 - 100 * position}%)`
+        next.style.transform = `translateX(${- 100 * nextPosition}%)`
+
+        position = nextPosition
+
+      }, 16)
+
+      setTimeout(nextPic, 3000)
+
+    }
+
+    return <div class = "carousel" >
+      {
+        this.data.map(url => {
+          let element = < img src = { url }
+          />
+          element.root.addEventListener("dragstart", e => e.preventDefault())
+          return element
+        })
+      } </div>
   }
   mountTo(parent) { // mount 是生命周期 一些DOM挂载最好也放入里面
-    this.slot = <div></div>
-    // parent.appendChild(this.root)
-    for (let child of this.children) {
-      // child.mountTo(this.root)  
-      this.slot.appendChild(child)
-    }
     this.render().mountTo(parent)
-
   }
 }
 
 
-let component = <MyComponent>text text text</MyComponent>
+let component = < Carousel data = {
+  [
+    "https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
+    "https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg",
+    "https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg",
+    "https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg",
+  ]
+}
+/>
 // let component = <div id="a" class="b" />
 
 component.mountTo(document.body)
 console.log(component)
-

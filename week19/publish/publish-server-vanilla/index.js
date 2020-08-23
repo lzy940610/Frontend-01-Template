e2eb142420ebbbddac3464892d708544e5e65e99
lizhenyu <985818479@qq.com>
2020-08-23 08:48:27 +0800

    第20周作业提交
    1、github oauth授权功能加入

--- week19/publish/publish-server-vanilla/index.js
+++ week19/publish/publish-server-vanilla/index.js
@@ -1,31 +1,128 @@
 const http = require('http');
 const fs = require('fs')
 const unzipper = require('unzipper')
-
+const https = require('https')
 // Create an HTTP server
 const server = http.createServer((req, res) => {
-  let matched = req.url.match(/filename=([^&]+)/)
-  let filename = matched && matched[1]
-  console.log(filename)
-  if (!filename) 
+  // let matched = req.url.match(/filename=([^&]+)/)
+  // let filename = matched && matched[1]
+  // if (!filename) return
+
+  // oauth
+  if (req.url.match(/^\/auth/)) {
+    return auth(req, res)
+  }
+
+  if (!req.url.match(/^\/?/)) {
+    res.writeHead(404, {
+      'Content-Type': 'text/plain'
+    })
+    res.end('not found')
     return
+  }
 
-  // let writeStream = fs.createWriteStream('../server/public/' + filename)
-  // let writeStream = fs.createWriteStream('../server/public/package.zip')
-  let writeStream = unzipper.Extract({ path: '../server/public' })
-  req.pipe(writeStream)
+  // 调用github api
+  const options = {
+    hostname: 'api.github.com',
+    port: 443,
+    path: '/user',
+    method: 'GET',
+    headers: {
+      Authorization: `token ${req.headers.token}`,
+      "User-Agent": "toy-publish-server"
+    }
+  };
 
+  const request = https.request(options, (response) => {
+    // console.log('statusCode:', res.statusCode);
+    // console.log('headers:', res.headers);
+    // 会分段 所以会接受多次
+    let body = ""
+    response.on('data', (d) => {
+      body += d.toString()
+    });
+    response.on('end', () => {
+      let user = JSON.parse(body)
+      console.log(user)
+      // 权限检查
 
-  // req.on('data', trunk => {
-  //   writeStream.write(trunk)
-  // })
-  // req.on('end', trunk => {
-  //   writeStream.end(trunk)
-  // })
-  req.on('end', () => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('okay')
-  })
+      // 开始接收数据
+      let writeStream = unzipper.Extract({
+        path: '../server/public'
+      })
+      req.pipe(writeStream)
+      // req.on('data', trunk => {
+      //   writeStream.write(trunk)
+      // })
+      // req.on('end', trunk => {
+      //   writeStream.end(trunk)
+      // })
+
+      req.on('end', () => {
+        res.writeHead(200, {
+          'Content-Type': 'text/plain'
+        })
+        res.end('okay')
+      })
+
+    })
+  });
+
+  request.on('error', (e) => {
+    console.error(e);
+  });
+  request.end();
+
 });
+
+function auth(req, res) {
+  // 授权
+  let code = req.url.match(/code=([^&]+)/)[1]
+  let state = "abc123"
+  let client_secret = "cdb35c308952e58da220f8a367b47c7350bbd458"
+  let client_id = "Iv1.86ec739f37e5fa74"
+  let redirect_uri = encodeURIComponent("http://localhost:8081/auth")
+
+  let params = `code=${code}&state=${state}&client_secret=${client_secret}&client_id=${client_id}&redirect_uri=${redirect_uri}`
+  let url = `https://github.com/login/oauth/access_token?${params}`
+  const options = {
+    hostname: 'github.com',
+    port: 443,
+    path: `/login/oauth/access_token?${params}`,
+    method: 'POST'
+  };
+
+  const request = https.request(options, (response) => {
+    // console.log('statusCode:', res.statusCode);
+    // console.log('headers:', res.headers);
+
+    response.on('data', (d) => {
+      console.log(d.toString())
+      // process.stdout.write(d);
+      let result = d.toString().match(/access_token=([^&]+)/)
+      if (result) {
+        let token = result[1]
+        res.writeHead(200, {
+          'access_token': token,
+          'Content-Type': 'text/html'
+        })
+        // res.end('okay')
+        res.end(`<a href="http://localhost:8080/publish?token=${token}">publish</a>`)
+      } else {
+        res.writeHead(200, {
+          'Content-Type': 'text/plan'
+        })
+        res.end('error')
+      }
+
+    });
+
+  });
+
+  request.on('error', (e) => {
+    console.error(e);
+  });
+  request.end();
+}
 
 server.listen(8081)
--- week19/publish/publish-tool/publish.js
+++ week19/publish/publish-tool/publish.js
@@ -3,16 +3,28 @@ const querystring = require('querystring')
 const { fstat } = require('fs')
 const fs = require('fs')
 const archiver = require('archiver');
+const child_process = require('child_process')
 
 let packname = './package'
-
-fs.stat(packname, (error, stat) => {
+/**
+ * Iv1.86ec739f37e5fa74
+ * cdb35c308952e58da220f8a367b47c7350bbd458
+ */
+let client_id = 'Iv1.86ec739f37e5fa74'
+let client_secret = 'cdb35c308952e58da220f8a367b47c7350bbd458'
+let redirect_uri = encodeURIComponent("http://localhost:8081/auth")
+child_process.exec(`open https://github.com/login/oauth/authorize?client_id=${client_id}&client_secret=${client_secret}&redirect_uri=${redirect_uri}`)
+// fs.stat(packname, (error, stat) => {
+const server = http.createServer((request, res) => {
+    let token = request.url.match(/token=([^&]+)/)[1]
+    console.log('real publish!!')
     const options = {
         hostname: 'localhost',
         port: 8081,
-        path: `/?filename=${packname}.zip`,
+        path: `/?filename=package.zip`,
         method: 'POST',
         headers: {
+            'token': token,
             'Content-Type': 'application/octet-stream'
         }
     };
@@ -38,55 +50,12 @@ fs.stat(packname, (error, stat) => {
 
     archive.on('end', () => {
         req.end()
+        console.log('publish success!')
+        server.close()
     })
 
-
-    // Write data to request body
-    // let readStream = fs.createReadStream('./' + filename)
-    // readStream.pipe(req)
-    // readStream.on('end', () => {
-    //     req.end()
-    // })
-    // req.write(postData);
-    // req.end();
 })
 
+server.listen(8080)
 
 
-
-// !!!!!!!!!!!!!!!!!!
-
-// const postData = querystring.stringify({
-//     'msg': 'Hello World!'
-// });
-
-// const options = {
-//     hostname: 'www.google.com',
-//     port: 80,
-//     path: '/upload',
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/x-www-form-urlencoded',
-//         'Content-Length': Buffer.byteLength(postData)
-//     }
-// };
-
-// const req = http.request(options, (res) => {
-//     console.log(`STATUS: ${res.statusCode}`);
-//     console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-//     res.setEncoding('utf8');
-//     res.on('data', (chunk) => {
-//         console.log(`BODY: ${chunk}`);
-//     });
-//     res.on('end', () => {
-//         console.log('No more data in response.');
-//     });
-// });
-
-// req.on('error', (e) => {
-//     console.error(`problem with request: ${e.message}`);
-// });
-
-// // Write data to request body
-// req.write(postData);
-// req.end();
